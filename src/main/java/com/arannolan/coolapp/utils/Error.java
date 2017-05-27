a2d362e48b9f0af685790b06b54f4aa279d7bd55
Aran Nolan <nolanar@tcd.ie>
2017-05-27 22:21:07 +0100

    Add util class containing error messages

---
+++ src/main/java/com/arannolan/coolapp/utils/Error.java
@@ -0,0 +1,11 @@
+package com.arannolan.coolapp.utils;
+
+/**
+ * Created by aran on 27/05/17.
+ */
+public class Error {
+    public static final String USER_NOT_FOUND = "User does not exist";
+    public static final String INVALID_TOKEN = "Malformed user access token";
+    public static final String MISSING_PERMISSIONS = "Missing required permissions";
+
+}
